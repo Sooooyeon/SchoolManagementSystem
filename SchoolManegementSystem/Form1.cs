1d2212d5f21908e0fa201c863a6ce179cd38e538
Sooooyeon <tn7278@naver.com>
2024-07-27 13:27:55 +0900

    Design: 랜딩 화면 추가

---
+++ SchoolManegementSystem/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolManegementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTitle.Left = (this.ClientSize.Width - lblTitle.Width) / 2;
+            pictureBox1.Left = (this.ClientSize.Width - pictureBox1.Width) / 2;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel2.Width += 6;
+
+            if (panel2.Width >= 525) timer1.Stop();
+        }
+    }
+}
